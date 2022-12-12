6594c7d1c027bcc9d7c973972b2fbdb05d6bd885
ivan l <317868+iloire@users.noreply.github.com>
2022-12-12 18:29:17 +0000

    chore: cleanup scripts (now using ansible)

--- scripts/install-hosts-blocklist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# https://someonewhocares.org/hosts/
-sudo curl https://someonewhocares.org/hosts/hosts --output /etc/hosts
--- scripts/install-npm-global-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm install -g diff-so-fancy
-npm install -g yarn
-npm install -g cloc
-npm install -g prettier
