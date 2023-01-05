b1027ebc8b95cce0fa4a7db707f5a27d6a6bddc0
ivan l <317868+iloire@users.noreply.github.com>
2023-01-05 22:04:47 +0000

    chore: cleanup

--- nvim/lua/ivan/snippets/all.lua
+++ nvim/lua/ivan/snippets/all.lua
@@ -5,12 +5,6 @@ local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 
-ls.add_snippets("lua", {
-	s("ternary lua", {
-		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
-	})
-})
-
 ls.add_snippets("all", {
 	s("ternary", {
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
