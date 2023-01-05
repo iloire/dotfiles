86690218b3efc4720dd2b2b95821a0b4335387aa
ivan l <317868+iloire@users.noreply.github.com>
2023-01-05 19:14:14 +0000

    feat: add fat arrow snippet

--- nvim/lua/ivan/snippets/javascript.lua
+++ nvim/lua/ivan/snippets/javascript.lua
@@ -9,14 +9,21 @@ ls.add_snippets("javascript", {
 	s("throw", {
     t("throw new Error('"), i(1, "error"), t("')")
 	}),
+
 	s("error", {
     t("console.error('"), i(1, "log"), t("')")
 	}),
+
 	s("log", {
     t("console.log("), i(1, ""), t(")")
 	}),
+
 	s("ternary javascript", {
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
-	})
+	}),
+
+	s("fat", {
+    t("const "), i(1, "name"), t(" = ("), i(2, ""), t(") => {"), i(3, ""), t("}")
+	}),
 })
 
