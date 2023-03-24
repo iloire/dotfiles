6478096b0036a15917d8cc0678bad48756837960
ivan l <317868+iloire@users.noreply.github.com>
2023-03-24 07:05:26 +0000

    feat: auto close nvim-tree if last buffer is closed

--- nvim/lua/ivan/autocmd.lua
+++ nvim/lua/ivan/autocmd.lua
@@ -4,6 +4,29 @@ MyGroup = augroup("mygroup", {})
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup("HighlightYank", {})
 
+-- nvim-tree is also there in modified buffers so this function filter it out
+-- https://github.com/nvim-tree/nvim-tree.lua/issues/1005
+local modifiedBufs = function(bufs)
+    local t = 0
+    for k,v in pairs(bufs) do
+        if v.name:match("NvimTree_") == nil then
+            t = t + 1
+        end
+    end
+    return t
+end
+
+autocmd({"BufEnter"}, {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and
+        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+            vim.cmd "quit"
+        end
+    end
+})
+
 autocmd({"BufWritePre"}, {
     group = MyGroup,
     pattern = "*",
