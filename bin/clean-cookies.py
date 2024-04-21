37f40b6f135436de3b7528cf7a89d3be517768d3
ivan l. <317868+iloire@users.noreply.github.com>
2024-04-21 09:44:59 +0100

    chore: log to file deleted rows

--- bin/clean-cookies.py
+++ bin/clean-cookies.py
@@ -14,7 +14,14 @@ www.namecheap.com
 import sqlite3 as db
 import os
 import sys
+import datetime
 from sys import platform
+
+def append_to_log(log_file, deleted_rows):
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_line = f"{current_date} - deleted rows: {len(deleted_rows)}\n"
+    with open(log_file, "a") as file:
+        file.write(log_line)
 
 home_dir = os.path.expanduser('~')
 whitelist_dir = f'{home_dir}/cookies-whitelist.txt'
@@ -42,16 +49,22 @@ run_clean = (len(sys.argv)> 1 and sys.argv[1] == '--clean')
 print(f"Number cookies: {len(rows)}")
 print(f"runclean: {run_clean}")
 
+deleted_rows = []
+
 for index, row in enumerate(rows):
     host = row[0]
     if (host not in white_listed_lines):
         if (run_clean):
             c.execute("DELETE FROM cookies where host_key='" + host + "'")
+            deleted_rows.append(host)
         else:
             print(f"dry run: skipping removing cookie for {host}")
     else: # white listed
         if not (run_clean):
             print(f"white listed: {host}")
+
+log_file = f'{home_dir}/cookies-whitelist-log.log'
+append_to_log(log_file, deleted_rows)
 
 conn.commit()
 c.execute("vacuum")
