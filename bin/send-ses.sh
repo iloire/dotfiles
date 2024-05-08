ccea600a95dadb92c3287a66186a26f543246c06
ivan l <317868+iloire@users.noreply.github.com>
2024-05-08 23:30:53 +0100

    feat: add send-ses.sh

---
+++ bin/send-ses.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# 1. Create a file like:
+# ~/.aws/credentials
+#[email]
+#aws_access_key_id=
+#aws_secret_access_key=
+#
+#
+# 2. Need to verify email address in AWS console *for that particular region*
+#
+EMAIL=$ADMIN_EMAIL
+REGION=us-east-1
+SUBJECT=$1
+BODY=$2
+
+echo $SUBJECT $BODY
+aws --profile email ses send-email --from $EMAIL --to $EMAIL --region $REGION --text "$BODY" --subject "$SUBJECT"
