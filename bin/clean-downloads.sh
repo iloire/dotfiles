107a0fc63058e75f79bfb2bad14c687600e6b3eb
ivan l <317868+iloire@users.noreply.github.com>
2024-05-29 01:32:34 +0100

    fix: fix clean-downloads.sh (was not working on Ubuntu)

--- bin/clean-downloads.sh
+++ bin/clean-downloads.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -d \"$HOME/Downloads/\" ] && find \"$HOME/Downloads/\" -maxdepth 1 -mtime +4 -type f -delete
+[ -d "$HOME/Downloads/" ] && find "$HOME/Downloads/" -maxdepth 1 -mtime +4 -type f -delete
