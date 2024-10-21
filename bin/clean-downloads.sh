7424bb3de12094fc263d082d3cf910643df8ee90
ivan l. <317868+iloire@users.noreply.github.com>
2024-10-21 22:19:42 +0100

    chore: delete zip archives sooner

--- bin/clean-downloads.sh
+++ bin/clean-downloads.sh
@@ -3,5 +3,8 @@
 [ -d "$HOME/Downloads/installers" ] && find "$HOME/Downloads/installers" -maxdepth 1 -mtime +4 -type f -delete
 [ -d "$HOME/Downloads/images" ] && find "$HOME/Downloads/images" -maxdepth 1 -mtime +10 -type f -delete
 [ -d "$HOME/Downloads/torrents" ] && find "$HOME/Downloads/torrents" -maxdepth 1 -mtime +10 -type f -delete
+
+#archives
 [ -d "$HOME/Downloads/archives" ] && find "$HOME/Downloads/archives" -maxdepth 2 -mtime +10 -type f -delete
+[ -d "$HOME/Downloads/archives" ] && find "$HOME/Downloads/archives" -maxdepth 2 -mtime +2 -type f -name "*.zip" -delete
 [ -d "$HOME/Downloads/archives" ] && find "$HOME/Downloads/archives" -maxdepth 2 -mtime +10 -type d -exec rm -r {} \;
