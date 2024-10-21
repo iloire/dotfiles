7a813029e4ad8e102b4003191f69b3cfdab921f4
ivan l. <317868+iloire@users.noreply.github.com>
2024-10-21 22:16:56 +0100

    chore: more granular clean downloads including archives folder

--- bin/clean-downloads.sh
+++ bin/clean-downloads.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 [ -d "$HOME/Downloads/" ] && find "$HOME/Downloads/" -maxdepth 1 -mtime +4 -type f -delete
+[ -d "$HOME/Downloads/installers" ] && find "$HOME/Downloads/installers" -maxdepth 1 -mtime +4 -type f -delete
+[ -d "$HOME/Downloads/images" ] && find "$HOME/Downloads/images" -maxdepth 1 -mtime +10 -type f -delete
+[ -d "$HOME/Downloads/torrents" ] && find "$HOME/Downloads/torrents" -maxdepth 1 -mtime +10 -type f -delete
+[ -d "$HOME/Downloads/archives" ] && find "$HOME/Downloads/archives" -maxdepth 2 -mtime +10 -type f -delete
+[ -d "$HOME/Downloads/archives" ] && find "$HOME/Downloads/archives" -maxdepth 2 -mtime +10 -type d -exec rm -r {} \;
